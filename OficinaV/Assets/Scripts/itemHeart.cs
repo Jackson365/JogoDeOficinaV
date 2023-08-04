9503fd38d361d5f79ff38b14f10652393494fa0a
bruna <bruna.paixao@escolar.ifrn.edu.br>
2023-08-04 19:01:14 -0300

    vidas do player

---
+++ OficinaV/Assets/Scripts/itemHeart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemHeart : MonoBehaviour
+{
+    public int valueHaelth;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().IncreaseLife(valueHaelth);
+            Destroy(gameObject);
+        }
+    }
+}
